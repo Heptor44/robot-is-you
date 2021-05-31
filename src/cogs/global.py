90f15a38ca30185b79060af5047e84a98250904b
Balt <59123926+Heptor44@users.noreply.github.com>
2021-05-31 18:32:18 -0500

    Code shorten in tiling checking
    From "0", "1"... "15" to (str(n) for n in range(0,16))

--- src/cogs/global.py
+++ src/cogs/global.py
@@ -509,12 +509,7 @@ class GlobalCog(commands.Cog, name="Baba Is You"):
                                 else:
                                     raise FileNotFoundError(word)
                             elif tiling == "1":
-                                if variant in (
-                                    "0", "1", "2", "3",
-                                    "4", "5", "6", "7",
-                                    "8", "9", "10", "11",
-                                    "12", "13", "14", "15",
-                                ):
+                                if variant in (str(n) for n in range(0,16)):
                                     final.variant = variant
                                 elif is_level:
                                     direction = 0
